c4c1764071f000ab9946d405783eefe23d890944
ivan.a.oliveira@terra.com.br <ivan.a.oliveira@terra.com.br>
2019-07-08 19:46:15 -0300

    fix pc

--- app/Controllers/Http/ReceberController.js
+++ app/Controllers/Http/ReceberController.js
@@ -1,6 +1,6 @@
 'use strict'
 
-// const Receber = use('App/Models/Receber')........ pc
+// const Receber = use('App/Models/Receber')........ pc 01
 const Database = use('Database')
 
 const ServiceReceber = use('App/Services/Receber')
