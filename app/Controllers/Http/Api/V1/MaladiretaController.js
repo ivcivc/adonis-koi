1559de0032d51be0431d5160f624a032e92eb519
ivan.a.oliveira@terra.com.br <ivan.a.oliveira@terra.com.br>
2019-08-30 20:10:29 -0300

    fix mala-direta

--- app/Controllers/Http/Api/V1/MaladiretaController.js
+++ app/Controllers/Http/Api/V1/MaladiretaController.js
@@ -11,29 +11,30 @@ class MaladiretaController {
     try {
       const payload = request.all()
 
-      const query = Database.table('eventos')
+      const query = Database.table('participantes')
 
         .select(
-          'eventos.nome as evento',
-          'treinamento_id',
           'pessoa_id',
           'pessoas.nome as pessoa',
           'pessoas.cpf',
           'pessoas.email',
           'pessoas.sexo',
           'pessoas.dnasc',
-          'pessoas.estado_civil',
-          'treinamentos.nome as treinamento'
+          'pessoas.estado_civil'
         )
-        .innerJoin('participantes', 'eventos.id', 'participantes.evento_id')
-        .innerJoin('treinamentos', 'eventos.treinamento_id', 'treinamentos.id')
+        // .innerJoin('participantes', 'eventos.id', 'participantes.evento_id')
+        // .innerJoin('treinamentos', 'eventos.treinamento_id', 'treinamentos.id')
         .innerJoin('pessoas', 'participantes.pessoa_id', 'pessoas.id')
 
       if (payload.estado_civil) {
         query.whereIn('pessoas.estado_civil', payload.estado_civil)
       }
       if (payload.treinamento_id) {
-        query.whereIn('eventos.treinamento_id', payload.treinamento_id)
+        query
+          .count('* as count')
+          .whereIn('evento_id', payload.treinamento_id)
+          .groupBy('pessoa_id')
+          .having('count', '>=', payload.treinamento_id.length)
       }
 
       if (payload.sexo) {
@@ -41,10 +42,26 @@ class MaladiretaController {
       }
 
       if (payload.not_treinamento_id) {
-        query.whereNotIn('eventos.treinamento_id', payload.not_treinamento_id)
+        // query.whereNotIn('eventos.treinamento_id', payload.not_treinamento_id)
+        query
+          .whereNotIn('pessoa_id', function () {
+            this.from('participantes')
+              .select('pessoa_id')
+              .whereIn('evento_id', payload.not_treinamento_id)
+              .groupBy('pessoa_id')
+              .havingRaw(
+                'count(pessoa_id) >= ?',
+                payload.not_treinamento_id.length
+              )
+          })
+          .groupBy('pessoa_id')
+      }
+      // query.where(1)
+      if (!payload.not_treinamento_id && !payload.treinamento_id) {
+        query.groupBy('pessoa_id')
       }
 
-      const r = await query.groupBy('pessoas.nome').orderBy('pessoas.nome')
+      const r = await query.orderBy('pessoas.nome') // .groupBy('pessoas.nome')
 
       response.status(200).send({ data: r })
 
